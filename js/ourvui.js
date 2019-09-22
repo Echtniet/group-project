8625bbff563b3e04301e753f7c03a3cff3a60455
Clinton Davelaar <DavelaarProgramming@gmail.com>
2019-09-22 18:12:51 -0500

    yessir

--- js/ourvui.js
+++ js/ourvui.js
@@ -1,6 +1,6 @@
 var myObject = new Vue({
 	el: '#app',
-	data: {name: 'Rando', score:Math.floor(Math.random() * 100) + 1,
+	data: {name: randomName(), score:Math.floor(Math.random() * 100) + 1,
 	students: [{name: 'Clinton', score:'98'}, 
 				{name: 'Cole', score: '101'},
 				{name: 'Michael', score:'6'},
@@ -8,6 +8,11 @@ var myObject = new Vue({
 				{name: 'Jacob', score:'89'}
 	]}
 })
+
+function randomName(){
+	let names = ['Michael', 'Denise', 'Charles', 'Nathan', 'Aziz', 'Scott', 'Badami', 'Doug'];
+	return names[Math.floor(Math.random() * 8)]
+}
 
 function addStudent() {
 	myObject.students.push({name:myObject.name, score:myObject.score})
